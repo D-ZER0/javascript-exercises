b2a6cbabc44df0099bf333f1a200e39736993ac0
Dave Ostergren <d.william.ostergren@gmail.com>
2022-08-10 14:39:36 -0400

    completed lessons 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    if (number > -1) {
+        let result = "";
+        for (let i = 0; i < number; i++) {
+            result = result.concat(string)
+        }
+        return result
+    } else {
+        return "ERROR"
+    }
 
 };
+
+// repeatString("hey", 3)
 
 // Do not edit below this line
 module.exports = repeatString;
