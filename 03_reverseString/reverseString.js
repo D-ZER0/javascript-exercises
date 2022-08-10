af2895cc3ca366f64465c5680e571c7958b10bc4
Dave Ostergren <d.william.ostergren@gmail.com>
2022-08-10 15:35:14 -0400

    completed lesson 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+function reverseString(string) {
+    let characters = string.split("")
+    let n = characters.length
+    let result = ""
+    for (let i = n-1; i >= 0; i--) {
+         result = result.concat(characters[i])
+    }
+    return result
 };
 
 // Do not edit below this line
