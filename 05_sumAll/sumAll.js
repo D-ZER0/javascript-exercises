213e2b19dd789cd80245ac99af5d0133396edea5
Dave Ostergren <d.william.ostergren@gmail.com>
2022-08-11 13:35:58 -0400

    completed lesson 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(low, high) {
+    if (typeof(low) != "number") {
+        return "ERROR"
+    } else if (typeof(high) != "number") {
+        return "ERROR"
+    } else if (low < 0) {
+        return "ERROR"
+    } else if (high < 0) {
+        return "ERROR"
+    } else if (low > high) {
+        let array = [];
+        for (let i = high; i <= low; i++) {
+            array.push(i)
+        }
+        sum = array.reduce((a, b) => a + b, 0)
+    } else {
+        let array = [];
+        for (let i = low; i <= high; i++) {
+            array.push(i)
+        }
+        sum = array.reduce((a, b) => a + b, 0)
 
+    } 
+    return sum
 };
 
 // Do not edit below this line
