ba0e4f959bb61e0df1fff23307dcfea368ffe83e
Dave Ostergren <d.william.ostergren@gmail.com>
2022-08-11 12:14:52 -0400

    completed lesson 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let result = array
+    // const args = Array.from(arguments);
+    for (arg of arguments) {
+        if (result.includes(arg)) {
+            let i = result.indexOf(arg);
+            result.splice(i, 1);
+        }
+    }
+return result;
+
 
 };
+
+// removeFromArray([1,2,3,4], 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
